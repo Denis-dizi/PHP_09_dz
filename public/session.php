26b02c838428f40df74e91c816ccd42625568c5d
Deniss Zhuravskis <davidovzzz@gmail.com>
2019-10-07 20:14:39 +0300

    session.php update

--- public/session.php
+++ public/session.php
@@ -1,17 +1,26 @@
 <?php
-    //2019.10.04
-     //before any code we start sesssion with session_start
-     session_start();
-     if (isset($_SESSION['myname'])) {
-         echo "Hello " . $_SESSION['myname'];
-     } else {
-         echo "I do not know you! please login";
-     }
-     echo "<hr>";
-     echo "<form action='login.php' method='post'>";
-     echo "<input name='user'>";
-     echo "<button name='submitbtn' type='submit'>LOGIN</button>";
-     echo "</form>";
-
-
-     
+    //before any code we start sesssion with session_start
+    session_start();
+    if (isset($_COOKIE['TestCookie'])) {
+        // echo "Aha! Your cookie name is: ".$_COOKIE['TestCookie'];
+        // echo "<br>";
+        $_SESSION['myname'] = $_COOKIE['TestCookie'];
+        //rememember we would need another cookie for the user id or
+        // we would need to get the user id from the database
+    }
+    if (isset($_SESSION['myname'])) {
+        echo "Hello " . $_SESSION['myname'];
+    } else {
+        echo "I do not know you! please login";
+    }
+    echo "<hr>";
+    echo "<form action='login.php' method='post'>";
+    echo "<label for='user-inp'>USERNAME</label>";
+    echo "<input name='user' id='user-inp'>";
+    echo "<label for='pw-inp'>PASSWORD</label>";
+    echo "<input name='pw' type='password' id='pw-inp'>";
+    echo "<button name='loginbtn' type='submit'>LOGIN</button>";
+    echo "<button name='logoutbtn' type='submit'>LOGOUT</button>";
+    echo "<input type='checkbox' name='remember' id='remember-chk'>";
+    echo "<label for='remember-chk'>Remember me (uses Cookie insert GDPR warning here)</label>";
+    echo "</form>";
