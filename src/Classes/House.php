2fb937d7fdbd3133d1ac15cca8408d091b590a08
Deniss Zhuravskis <davidovzzz@gmail.com>
2019-10-08 10:59:18 +0300

    Added class creation and instances no Vald

---
+++ src/Classes/House.php
@@ -0,0 +1,11 @@
+<?php
+class House {
+	public $primaryColor = 'black';
+	public $secondaryColors = [
+		'bathroom' => 'white',
+		'bedroom' => 'light pink',
+		'kitchen' => 'light blue'
+	];
+	public $hasPool = false;
+	public $extra;
+}
