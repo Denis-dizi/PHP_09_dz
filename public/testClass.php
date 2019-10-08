11a1e4d2ee8f32fa26a468f3aee8b8f153887647
Deniss Zhuravskis <davidovzzz@gmail.com>
2019-10-08 11:31:00 +0300

    updated from Val

--- public/testClass.php
+++ public/testClass.php
@@ -1,18 +1,29 @@
 <?php
     require_once("../src/Classes/House.php");
-    echo "Testing Classes";
+    echo "Testing Classes<hr>";
     
     //we create new instances(object) of the House
     $myhouse = new House();
     $house2 = new House();
     //we can access public properties with -> 
-    echo "My house " . $myhouse->primaryColor;
+    echo "My house " . $myhouse->getColor();
     echo "<hr>";
     //we can modify public properties directly even though it against OOP principles
-    echo "Other house " . $house2->primaryColor;
+    echo "Other house " . $house2->getColor();
     echo "<hr>";
-    $house2->primaryColor = "Green";
-    echo $house2->primaryColor;
+    //fix this line so we can change a private property using a method
+    // $house2->primaryColor = "Green";
+    $house2->setColor("coolnewcolor");
+    echo $house2->getColor();
     echo "<hr>";
-    echo "My house " . $myhouse->primaryColor;
-    echo "<hr>"; 
+    echo "My house " . $myhouse->getColor();
+    echo "<hr>";
+    $myhouse->greetMe("Valdis");
+    $myhouse->showColor();
+    $house2->showColor();
+    $myhouse->setColor("blue");
+    $house2->setColor("red");
+    $house2->setColor("violet");
+    // $myhouse->showColor();
+    // $house2->showColor();
+    echo "My house color is " . $myhouse->getColor() . "<br>";
--- src/Classes/House.php
+++ src/Classes/House.php
@@ -1,11 +1,27 @@
 <?php
-class House {
-	public $primaryColor = 'black';
-	public $secondaryColors = [
-		'bathroom' => 'white',
-		'bedroom' => 'light pink',
-		'kitchen' => 'light blue'
-	];
-	public $hasPool = false;
-	public $extra;
-}
+    class House {
+        //do stuff, properties and methods follow here
+        private $primaryColor = 'black';
+        public $secondaryColors = [
+            'bathroom' => 'white',
+            'bedroom' => 'light pink',
+            'kitchen' => 'light blue'
+        ];
+        public $hasPool = false;
+        public $extra;
+        public function greetMe($name) {
+            echo "Hello " . $name . "<br>";
+        }
+        public function showColor() {
+            echo "Your house color is " . $this->primaryColor ."<br>";
+        }
+        //getter example 
+        public function getColor() {
+            return $this->primaryColor;
+        }
+        //setter example
+        public function setColor($newcolor) {
+            $this->primaryColor = $newcolor;
+            $this->showColor();
+        }
+    }
