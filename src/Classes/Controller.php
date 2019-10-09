b7773bd55067eae691e649f4c0a8c601ab60747a
Deniss Zhuravskis <davidovzzz@gmail.com>
2019-10-09 10:32:24 +0300

    upd "added get/post routing"

--- src/Classes/Controller.php
+++ src/Classes/Controller.php
@@ -5,8 +5,20 @@
         public function __construct(Model $newmodel) {
             $this->model = $newmodel;
         }
+        //we will start the page by routing
         public function route() {
-            //TODO add POST and GET processing
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $this->postReq();
+            }
+            if($_SERVER['REQUEST_METHOD'] == 'GET') {
+                $this->getReq();
+            }
+        }
+        private function postReq() {
+            //we process our post Requests here
+        }
+        private function getReq() {
+            //we process our get Requests here
             $this->model->processData();
         }
     }
